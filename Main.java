3ef0ab62a073e3d043fa93479c3cd44fc9832469
JosephKhateri <nukeow123@gmail.com>
2023-04-12 17:15:34 -0400

    Started Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+    private static final int ROWS = 10;
+    private static final int COLUMNS = 10;
+    private static final int MINE_COUNT = 10;}
