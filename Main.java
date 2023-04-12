936caa176e1dbdbe3e72465c35a3d1aafe249d8b
JosephKhateri <nukeow123@gmail.com>
2023-04-12 17:18:28 -0400

    Started Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ Initializing main class with fixed numbers of rows, columns, and mines
+ **/
 public class Main {
     private static final int ROWS = 10;
     private static final int COLUMNS = 10;
