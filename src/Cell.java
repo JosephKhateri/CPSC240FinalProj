1482cc35c5f1333f7b6ac6fc8f56b0a7c9e8cda4
JosephKhateri <nukeow123@gmail.com>
2023-04-13 11:09:33 -0400

    Added more variables

--- src/Cell.java
+++ src/Cell.java
@@ -3,4 +3,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Cell {
+    buttons = new JButton[ROWS][COLS];
+    mines = new boolean[ROWS][COLS];
+    revealed = new boolean[ROWS][COLS];
+    neighborCounts = new int[ROWS][COLS];
+
 }
