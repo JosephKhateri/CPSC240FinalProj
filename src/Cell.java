bbe9f862e9d6d2eea7444b59fb9c7232f9b0fc5b
ehuppumw <117955217+ehuppumw@users.noreply.github.com>
2023-04-16 16:47:05 -0400

    Eric Hupp Added changes

--- src/Cell.java
+++ src/Cell.java
@@ -3,7 +3,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Cell {
-
+    private int row;
+    private int col;
+    private boolean mine;
+    private boolean shown;
+    private int surroundingMines;
 
 
 }
