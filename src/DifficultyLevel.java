ea48fcb1ebe411371219eccef6841a0c73ac839e
kship <kshipey@gmail.com>
2023-04-18 10:45:18 -0400

    Keegan added to document

--- src/DifficultyLevel.java
+++ src/DifficultyLevel.java
@@ -2,5 +2,31 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class DifficultyLevel {
+public enum DifficultyLevel {
+    EASY(10, 10, 10),
+    MEDIUM(16, 16, 40),
+    HARD(16, 30, 99);
+
+    private final int rows;
+    private final int cols;
+    private final int mines;
+
+    DifficultyLevel(int rows, int cols, int mines) {
+        this.rows = rows;
+        this.cols = cols;
+        this.mines = mines;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getMines() {
+        return mines;
+    }
 }
+
