d918b837f8249ca814e56084a0378070dec3823d
ehuppumw <117955217+ehuppumw@users.noreply.github.com>
2023-04-18 11:04:26 -0400

    Eric Hupp Added changes

--- src/DifficultySelector.java
+++ src/DifficultySelector.java
@@ -4,4 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class DifficultySelector extends JDialog {
     private DifficultySelector selectedDifficulty;
+
+    public DifficultySelector(JFrame parent){
+        super(parent, "Select difficulty", true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setLayout(new GridLayout(3, 1));
+        JButton rookieButton = new JButton("rookie");
+        JButton experiencedButton = new JButton("experienced");
+        JButton VetButton = new JButton("Vet");
+
+
+    }
 }
