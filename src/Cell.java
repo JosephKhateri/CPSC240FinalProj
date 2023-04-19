58b93e6247edb8b0cd803da860bfce26ac710840
ehuppumw <117955217+ehuppumw@users.noreply.github.com>
2023-04-18 22:37:45 -0400

    Eric Hupp has added Changes

--- src/Cell.java
+++ src/Cell.java
@@ -24,16 +24,33 @@ public class Cell {
     }
 
 
-    public int getRow() {return row;}
-    public int getCol(){return col;}
-    public boolean hasMine(){ return mine; }
-    public void setMine(boolean mine){this.mine = mine;}
-    public boolean isShown(){ return shown; }
-    public void setShown(boolean shown){ this.shown = shown;}
-    public int getSurroundingMines(){return surroundingMines;}
+    public int getRow() {
+        return row;
+    }
+    public int getCol(){
+        return col;
+    }
+    public boolean hasMine(){
+        return mine;
+    }
+    public void setMine(boolean mine){
+        this.mine = mine;
+    }
+    public boolean isShown(){
+        return shown;
+    }
+    public void setShown(boolean shown)
+    { this.shown = shown;
+    }
+    public int getSurroundingMines(){
+        return surroundingMines;
+    }
     public void setSurroundingMines(int surroundingMines){this.surroundingMines = surroundingMines;}
 
+    public boolean isFlag() {
+        return flag;
     }
+}
 
 
 
