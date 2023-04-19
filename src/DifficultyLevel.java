05cb06fc5435a0f787764f609935293d38c788f9
ehuppumw <117955217+ehuppumw@users.noreply.github.com>
2023-04-18 20:57:52 -0400

    Eric Hupp has added Changes

--- src/DifficultyLevel.java
+++ src/DifficultyLevel.java
@@ -2,6 +2,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+// Telling each level sizes
 public enum DifficultyLevel {
     EASY(10, 10, 10),
     MEDIUM(16, 16, 40),
@@ -11,20 +12,21 @@ public enum DifficultyLevel {
     private final int cols;
     private final int mines;
 
+    // constructor for the enumeration
     DifficultyLevel(int rows, int cols, int mines) {
         this.rows = rows;
         this.cols = cols;
         this.mines = mines;
     }
-
+    // rows for the grade
     public int getRows() {
         return rows;
     }
-
+    // columns for the grid
     public int getCols() {
         return cols;
     }
-
+    // number of mines for the grid
     public int getMines() {
         return mines;
     }
